4ef3b6c69ca1c28230389503cc86ea3e8100f58d
vidanov <vidanov@gmail.com>
2024-07-12 13:53:32 +0200

    added OpenSearch alarms. added tfvars

--- opensearch-alarms/variables.tf
+++ opensearch-alarms/variables.tf
@@ -15,40 +15,8 @@ variable "alarms_email" {
   description = "An e-mail address for the alarms for the OpenSearch cluster"
 }
 
-
-
 variable "instance_count" {
   description = "The count of instances to be deployed"
   type        = number
   default     = 1
-}
-
-variable "master_instance_count" {
-  description = "The count of master instances"
-  type        = number
-  default     = 0
-}
-
-
-variable "warm_instance_count" {
-  description = "The count of warm instances"
-  type        = number
-  default     = 0
-}
-
-# Variable for enabling/disabling alarm groups
-variable "enable_alarm_groups" {
-  description = "Enable or disable groups of alarms"
-  type = object({
-    master_nodes  = bool
-    ultrawarm     = bool
-    cold_storage  = bool
-    plugin_alarms = bool
-  })
-  default = {
-    master_nodes  = true
-    ultrawarm     = true
-    cold_storage  = true
-    plugin_alarms = true
-  }
 }
--- opensearch-alarms/versions.tf
+++ opensearch-alarms/versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.58.0"
     }
   }
-  required_version = "~>1.7.5"
+  required_version = "~>1.9.1"
 }
 provider "aws" {
   region = var.region
